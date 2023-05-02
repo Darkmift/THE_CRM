1dae5be92ffb0942f0e2f56a087bd56902dfcb71
darkmift <darkmift@gmail.com>
2023-05-02 15:31:30 +0300

    code cleanup,added req.uuid,error handler,router

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,16 @@
-import express, { Express, Request, Response } from 'express';
+// express
+import express, { Express, NextFunction, Request, Response } from 'express';
+// libs
 import morgan from 'morgan';
 import dotenv from 'dotenv';
-import { NODE_ENV, PORT, LOG_FORMAT } from '@/config';
+import { v4 as uuidv4 } from 'uuid';
+
 import validateEnv from '@/utils/validateEnv';
+import errorHandler from '@/api/common/middlwares/error-handler';
+import mainRouter from '@/main.router';
+import { NODE_ENV, PORT, LOG_FORMAT } from '@/config';
 import { logger, stream } from '@/utils/logger';
-import { dataSource } from './db';
-import { User } from './entities/user';
-import { Role } from './entities/role';
-import { QueryFailedError } from 'typeorm/error/QueryFailedError';
+import { dataSource } from '@/db';
 
 validateEnv();
 dotenv.config();
@@ -15,146 +18,51 @@ dotenv.config();
 const app: Express = express();
 const port = PORT || 5000;
 
-(async () => {
-    try {
-        const appDataSource = await dataSource;
-        if (!appDataSource) throw new Error('DB INIT FAILED');
-        (globalThis as any).appDataSource = appDataSource;
-    } catch (error) {
-        console.log('🚀 ~ file: index.ts:21 ~ error:', error);
-        logger.error(`=DB INIT FAILED=`, error);
-        throw new Error('DB INIT FAILED');
-    }
-})();
+app.use(express.json());
 
 app.use(morgan(LOG_FORMAT, { stream }));
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('Express + TypeScript Server');
+app.all('*', (req: Request, res: Response, next: NextFunction) => {
+    req.uuid = uuidv4();
+    next();
 });
 
 app.get('/health', async (req: Request, res: Response) => {
     const data: any = {
+        uuid: req.uuid,
         uptime: process.uptime(),
         message: 'Ok',
         date: new Date(),
         sqlConnection: null,
     };
     try {
-        const roleRepository = appDataSource.getRepository(Role);
-        const userRepository = appDataSource.getRepository(User);
-
-        // Create user roles if they do not exist
-        const adminRole = await roleRepository.findOne({
-            where: { role: 'admin' },
-        });
-        const userRole = await roleRepository.findOne({
-            where: { role: 'user' },
-        });
-
-        if (!adminRole || !userRole) return;
-
-        // Create an array of 10 mock users
-        const mockUsers = [
-            {
-                firstName: 'Alice',
-                lastName: 'Smith',
-                email: 'alice.smith@example.com',
-                role: userRole,
-            },
-            {
-                firstName: 'Bob',
-                lastName: 'Johnson',
-                email: 'bob.johnson@example.com',
-                role: userRole,
-            },
-            {
-                firstName: 'Carol',
-                lastName: 'Williams',
-                email: 'carol.williams@example.com',
-                role: userRole,
-            },
-            {
-                firstName: 'David',
-                lastName: 'Brown',
-                email: 'david.brown@example.com',
-                role: adminRole,
-            },
-            {
-                firstName: 'Eve',
-                lastName: 'Jones',
-                email: 'eve.jones@example.com',
-                role: userRole,
-            },
-            {
-                firstName: 'Frank',
-                lastName: 'Garcia',
-                email: 'frank.garcia@example.com',
-                role: userRole,
-            },
-            {
-                firstName: 'Grace',
-                lastName: 'Miller',
-                email: 'grace.miller@example.com',
-                role: userRole,
-            },
-            {
-                firstName: 'Hank',
-                lastName: 'Davis',
-                email: 'hank.davis@example.com',
-                role: userRole,
-            },
-            {
-                firstName: 'Ivy',
-                lastName: 'Rodriguez',
-                email: 'ivy.rodriguez@example.com',
-                role: userRole,
-            },
-            {
-                firstName: 'Jack',
-                lastName: 'Martinez',
-                email: 'jack.martinez@example.com',
-                role: adminRole,
-            },
-        ];
-
-        // Insert the mock users into the database
-        await userRepository.save(mockUsers);
-
-        const result = await userRepository.find({ relations: ['role'] });
-        console.log('🚀 ~ file: index.ts:43 ~ app.get ~ result:', result);
+        const appDataSource = await dataSource;
+        if (!appDataSource) throw new Error('DB INIT FAILED');
+        await appDataSource.query('SELECT 1 + 1');
         data.sqlConnection = 'Ok';
-        data.users = result;
     } catch (error) {
-        if (error instanceof QueryFailedError) {
-            // Check for SQLite unique constraint error code
-            console.error(error);
-            if (error.driverError.errno === 19) {
-                data.sqlUniqueError = error.message;
-            } else {
-                console.error(
-                    'Query failed for another reason:',
-                    error.message,
-                );
-            }
-        } else {
-            console.error('Unknown error:', error);
-        }
-        if (error instanceof Error) {
-            data.sqlConnection = error.message;
-        }
-        data.sqlConnection = 'error validating sql structure';
+        logger.error('🚀 ~ api/health db conn error:', error);
+        data.sqlConnection = 'failed';
     }
 
     res.status(200).send(data);
 });
+
+app.use('/api', mainRouter);
+
+app.use((req: Request, res: Response) => {
+    res.status(404).json({ error: 'Resource not found' });
+});
+
+// Always last (global error handler)
+app.use(errorHandler);
 
 app.listen(port, () => {
     console.log(`⚡️[NODE_ENV]: ${NODE_ENV}`);
     console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
 
     logger.info(`=================================`);
-    logger.info(`======= ENV: ${NODE_ENV} =======`);
+    logger.info(`======= ENV: ${NODE_ENV} ========`);
     logger.info(`🚀 [server]: Server is running at http://localhost:${port}`);
     logger.info(`=================================`);
 });
