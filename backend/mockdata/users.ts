670debd008f3332df7df04aaee48cc3602e2590e
darkmift <darkmift@gmail.com>
2023-05-02 15:21:59 +0300

    added mockdata

---
+++ backend/mockdata/users.ts
@@ -0,0 +1,63 @@
+// Create an array of 10 mock users
+const mockUsers = [
+    {
+        firstName: 'Alice',
+        lastName: 'Smith',
+        email: 'alice.smith@example.com',
+        role: 'user',
+    },
+    {
+        firstName: 'Bob',
+        lastName: 'Johnson',
+        email: 'bob.johnson@example.com',
+        role: 'user',
+    },
+    {
+        firstName: 'Carol',
+        lastName: 'Williams',
+        email: 'carol.williams@example.com',
+        role: 'user',
+    },
+    {
+        firstName: 'David',
+        lastName: 'Brown',
+        email: 'david.brown@example.com',
+        role: 'admin',
+    },
+    {
+        firstName: 'Eve',
+        lastName: 'Jones',
+        email: 'eve.jones@example.com',
+        role: 'user',
+    },
+    {
+        firstName: 'Frank',
+        lastName: 'Garcia',
+        email: 'frank.garcia@example.com',
+        role: 'user',
+    },
+    {
+        firstName: 'Grace',
+        lastName: 'Miller',
+        email: 'grace.miller@example.com',
+        role: 'user',
+    },
+    {
+        firstName: 'Hank',
+        lastName: 'Davis',
+        email: 'hank.davis@example.com',
+        role: 'user',
+    },
+    {
+        firstName: 'Ivy',
+        lastName: 'Rodriguez',
+        email: 'ivy.rodriguez@example.com',
+        role: 'user',
+    },
+    {
+        firstName: 'Jack',
+        lastName: 'Martinez',
+        email: 'jack.martinez@example.com',
+        role: 'admin',
+    },
+];
