50839e2314a72820bc139a43a3a081479e956fe3
darkmift <darkmift@gmail.com>
2023-05-06 17:31:52 +0300

    jest configured correctly and test are being run

--- backend/jest.config.ts
+++ backend/jest.config.ts
@@ -1,16 +1,12 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 import type { Config } from '@jest/types';
 import { resolve } from 'path';
-const path = resolve(__dirname, '.env.development.local');
-console.log('🚀 ~ file: jest.config.ts:5 ~ path:', path);
 
 // Sync object
 const config: Config.InitialOptions = {
     testEnvironment: 'node',
     testEnvironmentOptions: {
         NODE_ENV: 'test',
-        // Set the environment variable for the dotenv path
-        DOTENV_CONFIG_PATH: resolve(__dirname, '.env.development.local'),
     },
     restoreMocks: true,
     coveragePathIgnorePatterns: [
@@ -20,30 +16,21 @@ const config: Config.InitialOptions = {
         'tests',
     ],
     roots: [resolve(__dirname)],
-    // // transform: {
-    // //     '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
-    // // },
-    globals: {
-        'ts-jest': {
-            tsconfig: {
-                baseUrl: './src',
-                paths: {
-                    '@/*': ['./*'],
-                },
-            },
-        },
-    },
-    // setupFiles: ['dotenv/config'],
-    setupFilesAfterEnv: [resolve(__dirname, '__test__', 'setup.test.ts')],
-    setupFiles: [resolve(__dirname, '__test__', 'setup.test.ts')],
-
-    transformIgnorePatterns: ['node_modules/(?!variables/.*)'],
-    // coverageReporters: ['text', 'lcov', 'clover', 'html'],
-    // modulePaths: [`<rootDir>`],
-    // moduleDirectories: ['node_modules', `<rootDir>/src`],
-    // moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
     moduleNameMapper: {
         '^@/(.*)$': '<rootDir>/src/$1',
+    },
+    transform: {
+        '^.+\\.(t|j)sx?$': [
+            'ts-jest',
+            {
+                tsconfig: {
+                    baseUrl: './src',
+                    paths: {
+                        '@/*': ['./*'],
+                    },
+                },
+            },
+        ],
     },
 };
 
