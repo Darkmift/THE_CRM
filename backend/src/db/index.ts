2fa69e55f5cdab8c228d41b959f3f33026b710c1
darkmift <darkmift@gmail.com>
2023-05-08 15:15:21 +0300

    rewrite file to encapsulate dataSource better

--- backend/src/db/index.ts
+++ backend/src/db/index.ts
@@ -3,8 +3,20 @@ import 'reflect-metadata'; // This import is required for TypeORM
 import { DataSource, ObjectType, Repository } from 'typeorm';
 import connectionOptions from '@/config/ormconfig';
 import { Role } from '@/entities/role';
+import { Year } from '@/entities/year';
 
-const connToDS = async () => {
+export async function testHealth() {
+    try {
+        const appDataSource = await dataSource;
+        if (!appDataSource) throw new Error('DB INIT FAILED');
+        await appDataSource.query('SELECT 1 + 1');
+        return true;
+    } catch (err) {
+        return false;
+    }
+}
+
+async function connToDS() {
     const dataSourceConn = new DataSource(connectionOptions);
     try {
         await dataSourceConn.initialize();
@@ -12,9 +24,9 @@ const connToDS = async () => {
 
         // Add this function to check and insert roles
         const checkAndInsertRoles = async () => {
-            const roleRepository = dataSourceConn.getRepository(Role);
+            const roleRepository = await dataSourceConn.getRepository(Role);
 
-            const requiredRoles = ['user', 'admin'];
+            const requiredRoles = ['user', 'admin', 'instructor', 'student'];
             for (const role of requiredRoles) {
                 const existingRole = await roleRepository.findOne({
                     where: { role },
@@ -28,20 +40,39 @@ const connToDS = async () => {
             }
         };
 
+        // Add this function to check and insert years
+        const checkAndInsertYears = async () => {
+            const yearRepository = await dataSourceConn.getRepository(Year);
+
+            const requiredYears = ['2022', '2021', '2023'];
+            for (const year of requiredYears) {
+                const existingYear = await yearRepository.findOne({
+                    where: { year },
+                });
+                if (!existingYear) {
+                    const insertedYear = new Year();
+                    insertedYear.year = year;
+                    await yearRepository.save(insertedYear);
+                    console.log(`Year "${year}" has been inserted.`);
+                }
+            }
+        };
+
         // Call the function to check and insert roles
         await checkAndInsertRoles();
+        await checkAndInsertYears();
 
         return dataSourceConn;
     } catch (err) {
         logger.error('Error during Data Source initialization', err);
         throw err;
     }
-};
+}
 
 export const dataSource = connToDS();
 
-export const getRepository = async <T>(Model: ObjectType<T>) => {
+export async function getRepository<T>(Model: ObjectType<T>) {
     const appDataSource = await dataSource;
     const targetRepo = appDataSource.getRepository(Model);
     return targetRepo as Repository<typeof Model>;
-};
+}
