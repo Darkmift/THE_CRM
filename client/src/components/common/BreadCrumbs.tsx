1c25dc799060fc4e4530c2142cecb0f4bb6450f7
darkmift <darkmift@gmail.com>
2023-05-21 17:09:39 +0300

    [crud-form-feature] added breadcrumbs cmp

---
+++ client/src/components/common/BreadCrumbs.tsx
@@ -0,0 +1,45 @@
+import { Breadcrumbs, Link as MuiLink, Typography } from '@mui/material';
+import { Link } from 'react-router-dom';
+
+import { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
+
+type BreadCrumbSegment = { path: string; name: string };
+
+function BreadCrumbs() {
+  const location = useLocation();
+  const [routeSegments, setRouteSegments] = useState<BreadCrumbSegment[]>([]);
+
+  useEffect(() => {
+    let cumulativePath = '';
+    const segments = location.pathname
+      .split('/')
+      .filter((s) => s.length)
+      .map((s) => {
+        cumulativePath += `/${s}`;
+        const name = s.replace(/-/g, ' ');
+        const formattedName = name.charAt(0).toUpperCase() + name.slice(1);
+        return { path: cumulativePath, name: formattedName };
+      });
+
+    setRouteSegments(segments);
+  }, [location]);
+
+  return (
+    <Breadcrumbs maxItems={2} separator="›" aria-label="breadcrumb">
+      {routeSegments.map((segment, key, self) => {
+        return segment === self[self.length - 1] ? (
+          <Typography key={key} color="text.primary">
+            {segment.name}
+          </Typography>
+        ) : (
+          <MuiLink component={Link} key={key} to={segment.path} underline="hover" color="inherit">
+            {segment.name}
+          </MuiLink>
+        );
+      })}
+    </Breadcrumbs>
+  );
+}
+
+export default BreadCrumbs;
